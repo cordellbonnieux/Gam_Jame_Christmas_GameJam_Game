c7f858d8584eeb07b514a96d9fe220d198d7e61a
cordell <cordellbonnieux@gmail.com>
2024-12-18 23:50:59 -0800

    slide rails can be approached from the bottom now

--- interactables/slide_edge/slide_edge.gd
+++ interactables/slide_edge/slide_edge.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
 @onready var end: Node2D = $zip_post
+@onready var collider: CollisionShape2D = $StaticBody2D/CollisionShape2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.zip(end, false)
+
+
+func _on_hide_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		collider.set_deferred("disabled", true)
+
+
+func _on_hide_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		collider.set_deferred("disabled", false)
