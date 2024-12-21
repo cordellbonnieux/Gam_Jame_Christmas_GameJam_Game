192366fb58025648c5237211a6fd0af6f007faa2
cordell <cordellbonnieux@gmail.com>
2024-12-20 20:57:07 -0800

    added anims to boss and got it in a working state

--- enemies/dog/boss_dog.gd
+++ enemies/dog/boss_dog.gd
@@ -8,34 +8,41 @@ var acceleration: float = 0.25
 @onready var fsm: StateMachine = $StateMachine
 @onready var close_ray: RayCast2D = $close_ray
 @onready var far_ray: RayCast2D = $far_ray
+@onready var ass_ray: RayCast2D = $ass_ray
+@onready var daze_timer: Timer = $daze_timer
 signal dead
 
 func _process(delta: float) -> void:
-	if abs(target.global_position.x - global_position.x) <= 228:
+	if fsm.current_state.name == "sleeping" && abs(target.global_position.x - global_position.x) <= 228:
 		wake_up()
 	
 	if (velocity.x > 0 && far_ray.target_position.x < 0) || (velocity.x < 0 && far_ray.target_position.x > 0):
 		flip_x()
 
 func _physics_process(delta: float) -> void:
-	if fsm.current_state.name == "moving":
-		# go towards target, if target is hooked, then go back and forth in room
-		if far_ray.is_colliding() && far_ray.get_collider() == target:
+	if fsm.current_state.name == "seeking":
+		if target.fsm.current_state.name == "hooking":
+			fsm.current_state.exit("moving")
+			
+		elif far_ray.is_colliding() && far_ray.get_collider() == target:
 			fsm.current_state.exit("pre-charging")
-		
-		elif target.fsm.current_state.name == "hooking":
-			if close_ray.is_colliding():
-				print(close_ray.get_collider())
-				velocity.x = lerp(velocity.x, -speed, acceleration)
-			else:
-				velocity.x = lerp(velocity.x, speed, acceleration)
-			move_and_slide()
-		
+			
 		else:
 			if target.global_position.x > global_position.x:
-				velocity.x = lerp(velocity.x, speed, acceleration)
-			else:
-				velocity.x = lerp(velocity.x, -speed, acceleration)
+				velocity.x += speed * delta
+			elif target.global_position.x < global_position.x:
+				velocity.x += -speed * delta
+			move_and_slide()
+			
+	elif fsm.current_state.name == "moving":
+		
+		if target.fsm.current_state.name != "hooking":
+			fsm.current_state.exit("seeking")
+			
+		else:
+			velocity.x += speed * delta
+			if close_ray.is_colliding() && close_ray.get_collider() != target:
+				velocity.x *= -1
 			move_and_slide()
 		
 	elif fsm.current_state.name == "charging":
@@ -47,7 +54,7 @@ func _physics_process(delta: float) -> void:
 			else:
 				fsm.current_state.exit("dazed")
 		else:
-			if target.global_position.x > global_position.x:
+			if far_ray.target_position.x > 0:
 				velocity.x = lerp(velocity.x, charge_speed, acceleration)
 			else:
 				velocity.x = lerp(velocity.x, -charge_speed, acceleration)
@@ -61,10 +68,11 @@ func flip_x() -> void:
 	close_ray.target_position.x *= -1
 	far_ray.target_position.x *= -1
 	anim.scale.x *= -1
+	ass_ray.target_position.x *= -1
 
 func wake_up() -> void:
 	if fsm.current_state.name == "sleeping":
-		fsm.current_state.exit("moving")
+		fsm.current_state.exit("seeking")
 
 
 func damage(amount: int = 1) -> void:
@@ -72,6 +80,7 @@ func damage(amount: int = 1) -> void:
 		health -= 1
 		if health <= 0:
 			fsm.current_state.exit("dead")
+			print("dead!")
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -80,24 +89,34 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	print("anim done")
 	if anim.animation == "pre-charging":
 		fsm.current_state.exit("charging")
-		
-	elif anim.animation == "attacking":
-		fsm.current_state.exit("moving")
+
+func _on_daze_timer_timeout() -> void:
+	fsm.current_state.exit("seeking")
 
 
 func _on_precharging_state_entered() -> void:
 	anim.play("pre-charging")
-	print("precharging")
 
 
 func _on_charging_state_entered() -> void:
 	anim.play("charging")
-	print("charging")
 
 
 func _on_dazed_state_entered() -> void:
 	anim.play("dazed")
-	print("dazed")
+	daze_timer.start()
+
+
+func _on_dead_state_entered() -> void:
+	anim.play("dead")
+	dead.emit()
+
+
+func _on_moving_state_entered() -> void:
+	anim.play("moving")
+
+
+func _on_seeking_state_entered() -> void:
+	anim.play("moving")
